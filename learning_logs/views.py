afca9ea41861166de1fe0d9f37367aec444118a5
Glushko-II <78683041+Glushko-II@users.noreply.github.com>
2021-04-01 23:04:05 +0300

    Added index.html and changed views.py.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """Доиашняя страница приложения Learning Log"""
+    return render(request, "learning_logs/index.html")
